dc32c78f84935a1a78a0ef67624894d50b7b516b
AddeMann <adrian.elias.mann@gmail.com>
2024-04-21 19:37:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
     install_requires = f.readlines()
     
 setup(
-    name='secrules',
+    name='python-secrules',
     version='1.0',
     packages=find_packages(),
     install_requires=install_requires
