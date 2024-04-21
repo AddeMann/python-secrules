822892af9d4bf25b19418d0ec94ba340920813e1
AddeMann <adrian.elias.mann@gmail.com>
2024-04-22 00:12:28 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
     install_requires = f.readlines()
     
 setup(
-    name='python-secrules',
+    name='secrules',
     version='1.0',
     packages=find_packages(),
+    package_dir={'secrules': 'secrules'},
+    package_data={"secrules": ["src/model/secrules.tx"]},
     install_requires=install_requires
 )
